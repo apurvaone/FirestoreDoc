21a4a8ec8e290bfb482b918d17af74f80ddb4579
apurvaone <57245377+apurvaone@users.noreply.github.com>
2021-11-22 16:09:16 +0530

    Firestore instance created and data added to firestore

--- app/src/main/java/com/example/firestoredoc/MainActivity.kt
+++ app/src/main/java/com/example/firestoredoc/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.firestoredoc
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val db = Firebase.firestore
+
+        val user= hashMapOf("first" to "Apurva",
+        "last" to "Shukla",
+        "dob" to "2001")
+
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+
+            }
+            .addOnFailureListener {
+                Log.w(TAG, "Error adding document", it)
+
+            }
+
+        // Create a new user with a first, middle, and last name
+        val user2 = hashMapOf(
+            "first" to "Alan",
+            "middle" to "Mathison",
+            "last" to "Turing",
+            "born" to 1912
+        )
+
+// Add a new document with a generated ID
+        db.collection("users")
+            .add(user2)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+
     }
 }
