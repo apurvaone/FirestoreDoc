af724f59d374295e2ed0763c71737416fb33a01c
apurvaone <57245377+apurvaone@users.noreply.github.com>
2021-11-23 19:09:50 +0530

    data update timesatamp added as a field

--- app/src/main/java/com/example/firestoredoc/MainActivity.kt
+++ app/src/main/java/com/example/firestoredoc/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -31,6 +32,12 @@ class MainActivity : AppCompatActivity() {
             .addOnFailureListener {
 
             }
+
+        val updates = hashMapOf<String, Any>(
+            "timestamp" to FieldValue.serverTimestamp()
+        )
+
+        idrref.update(updates).addOnCompleteListener {  }
 
 
 
